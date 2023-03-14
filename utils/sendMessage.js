4e46ccc794915dc834b79942599c00b5cf909941
diego-make <62081532+diego-maker@users.noreply.github.com>
2023-03-14 14:25:50 -0300

    adicionando o chatgpt

--- utils/sendMessage.js
+++ utils/sendMessage.js
@@ -1,24 +1,57 @@
 const axios = require("axios").default;
-const buttonWelcome = require('./Models/button-wellcome');
+const { ChatGPTAPI } = require('chatgpt')
+
+async function example(msg) {
+  const api = new ChatGPTAPI({
+    apiKey: process.env.OPENAI_API_KEY
+  })
+
+  const res = await api.sendMessage(msg)
+  return res.text
+
+}
 
 const sendMessage = (phone_number_id, from, msg_body, token) => {
 
-  const template =buttonWelcome.template 
-  axios({
-    method: "POST",
-    url:
-      "https://graph.facebook.com/v12.0/" +
-      phone_number_id +
-      "/messages?access_token=" +
-      token,
-    data: {
-      messaging_product: "whatsapp",
-      to: from,
-      type: "template",
-      template: template
-    },
-    headers: { "Content-Type": "application/json" },
-  });
+const sliptMSG = msg_body.split(" ");
+const countKeyMSG = sliptMSG.length;
+
+  if (countKeyMSG > 12 ) {
+   return axios({
+      method: "POST",
+      url:
+        "https://graph.facebook.com/v12.0/" +
+        phone_number_id +
+        "/messages?access_token=" +
+        token,
+      data: {
+        messaging_product: "whatsapp",
+        to: from,
+        text: { body: "A não essa mensagem é muito grande para eu processar por enquanto 😔, tente diminuir sua pergunta por gentileza"  },
+      },
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+  else{
+
+  const valueText = example(msg_body)
+
+    return axios({
+      method: "POST",
+      url:
+        "https://graph.facebook.com/v12.0/" +
+        phone_number_id +
+        "/messages?access_token=" +
+        token,
+      data: {
+        messaging_product: "whatsapp",
+        to: from,
+        text: { body: `Chatgpt🤖 \n ${valueText}`  },
+      },
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+ 
 };
 
 module.exports = { sendMessage };
