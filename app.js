4349c9fe5691a04a8a8899a7e90cbfd53fea6d7b
diego-make <62081532+diego-maker@users.noreply.github.com>
2023-03-14 11:31:44 -0300

    atualizando log

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.post("/webhook", (req, res) => {
   let body = req.body;
 
   // Check the Incoming webhook message
-  console.log(JSON.stringify(req.body, null, 2));
+  console.log(body);
 
   // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
   if (req.body.object) {
